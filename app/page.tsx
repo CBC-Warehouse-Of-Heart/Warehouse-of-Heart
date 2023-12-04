90843b961657339732a7baca7c0780136e808847
tontan2545 <tontan2545@gmail.com>
2023-12-04 20:10:01 +0700

    feat: prettier fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-full w-full justify-center items-center gap-10 sm:flex-row">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-10 sm:flex-row">
       <Button>พร้อมแล้ว</Button>
       <Button variant="secondary" className="">
         ยังไม่แน่ใจ
