046587c36efe6666b51c02e01fe82b87acaf2f25
tontan2545 <tontan2545@gmail.com>
2023-12-04 20:03:35 +0700

    feat: fix responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex h-full justify-center items-center space-x-10">
+    <div className="flex flex-col h-full w-full justify-center items-center gap-10 sm:flex-row">
       <Button>พร้อมแล้ว</Button>
-      <Button variant="secondary">ยังไม่แน่ใจ</Button>
+      <Button variant="secondary" className="">
+        ยังไม่แน่ใจ
+      </Button>
       <Button className="font-serif">Register</Button>
     </div>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center rounded-md text-xl hover:ring-4 hover:ring-ring font-medium transition-colors disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-md hover:ring-4 hover:ring-ring font-medium transition-colors disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -19,7 +19,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-auto px-6 py-3",
+        default: "h-auto text-xl sm:text-5xl px-6 py-3 sm:py-5",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
