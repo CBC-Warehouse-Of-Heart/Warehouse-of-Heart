948f44ae8733aaeaf5146d460a8644939ff55cd4
matikant <fe-2003@hotmail.com>
2023-12-15 12:48:40 +0700

    add page 1-1, 1-2, 1-3

---
+++ app/(interactive)/1-2/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation'
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const router = useRouter()
+  return (
+    <div className='flex h-full w-full flex-col items-center justify-center' onClick={() => router.push('/1-3')}>
+
+      <div className='flex flex-col items-center text-center text-[21px] text-[#F8F8F7] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] leading-relaxed'>
+        <p>เรากำลังจะพาเธอเข้าสู่<br/>เส้นทางการสำรวจ<br/>และทบทวนสิ่งที่อยู่ในใจ</p>
+      </div>
+
+      <p className="fixed bottom-36 text-[#F8F8F7] text-[15px]">กดที่หน้าจอเพื่อไปต่อ</p>
+
+    </div>
+  );
+};
+
+export default Page;
