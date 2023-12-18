7065b202c3f7c0573d1a991bb4a739f1a425425e
tontan2545 <tontan2545@gmail.com>
2023-12-18 19:32:24 +0700

    feat: tooltip to follow slider thumb

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { useState, useRef, } from "react";
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
@@ -12,35 +11,24 @@ const Slider = React.forwardRef<
 >(({ className, onValueChange, ...props }, ref) => {
   const [value, setValue] = React.useState(props.defaultValue || props.value);
 
-  const handleChange = (newValue:any) => {
+  const handleChange = (newValue: any) => {
     setValue(newValue);
     onValueChange && onValueChange(newValue);
   };
-  
-    // THis ref will be connected to the orange box
-    const boxRef = useRef<HTMLDivElement>(null);
-
-    // X
-    const [x, setX] = useState<number | undefined>();
-  
-    // Y
-    const [y, setY] = useState<number | undefined>();
-  
-    // This function calculate X and Y
-    const getPosition = () => {
-      const x = boxRef.current?.offsetLeft;
-      setX(x);
-  
-      const y = boxRef.current?.offsetTop;
-      setY(y);
-    };
-
 
   return (
-    <div className={cn("relative flex w-full touch-none select-none items-center", className)}>
+    <div
+      className={cn(
+        "relative flex w-full touch-none select-none items-center",
+        className,
+      )}
+    >
       <SliderPrimitive.Root
         ref={ref}
-        className={cn("relative flex w-full touch-none select-none items-center", className)}
+        className={cn(
+          "relative flex w-full touch-none select-none items-center",
+          className,
+        )}
         {...props}
         onValueChange={handleChange}
         value={value}
@@ -50,19 +38,13 @@ const Slider = React.forwardRef<
         </SliderPrimitive.Track>
         <SliderPrimitive.Thumb
           className="block h-7 w-7 rounded-full bg-light-purple-slider ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-          style={{ boxShadow: '0 4px 10px 5px rgba(0, 0, 0, 0.2)' }}
-        />
+          style={{ boxShadow: "0 4px 10px 5px rgba(0, 0, 0, 0.2)" }}
+        >
+          <div className="pointer-events-none absolute -left-1/2 -top-14 flex h-10 w-16 items-center justify-center rounded-lg bg-purple-slider text-center text-lg font-bold text-white">
+            {value}
+          </div>
+        </SliderPrimitive.Thumb>
       </SliderPrimitive.Root>
-
-
-      <div className="absolute top-0 left-0 mt-8 ml-2 px-2 py-1 bg-white text-black rounded shadow"
-      ref={boxRef}
-      style={{
-        width: 40, textAlign: 'center', left: 120
-      }}
-      >
-        {value}
-      </div>
     </div>
   );
 });
