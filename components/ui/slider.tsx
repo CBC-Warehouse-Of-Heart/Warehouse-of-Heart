f65fe088b46976838682d71e72e966131f346a77
RataSurithpinyo <rataprim30026@gmail.com>
2023-12-24 23:05:11 +0900

    [2-1a] change value box style

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -36,15 +36,49 @@ const Slider = React.forwardRef<
         <SliderPrimitive.Track className="relative h-4 w-full grow overflow-hidden rounded-full bg-woh-white">
           <SliderPrimitive.Range className="absolute h-full bg-purple-slider" />
         </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb
+        {/* <SliderPrimitive.Thumb
           className="block h-7 w-7 rounded-full bg-light-purple-slider ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
           style={{ boxShadow: "0 4px 10px 5px rgba(0, 0, 0, 0.2)" }}
         >
-          <div className="pointer-events-none absolute -left-1/2 -top-14 flex h-10 w-16 items-center justify-center rounded-lg bg-purple-slider text-center text-lg font-bold text-white">
+          <div className="bottom-[20px] text-lg font-bold text-white"
+          // style={{ backgroundImage: 'url("/img/value_box.png")', backgroundSize: 'cover' }}
+          style={{
+            backgroundImage: "url('/img/value_box.png')",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+         >
+          {value}
+          </div>
+        </SliderPrimitive.Thumb> */}
+
+        <SliderPrimitive.Thumb
+          className="block h-7 w-7 rounded-full bg-light-purple-slider ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+          style={{
+            boxShadow: "0 4px 10px 5px rgba(0, 0, 0, 0.2)",
+            position: "relative",
+          }}
+        >
+          <div
+            className="text-lg font-bold text-white"
+            style={{
+              position: "absolute",
+              top: "-40px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              backgroundImage: "url('/img/value_box.png')",
+              backgroundSize: "100% 100%",
+              backgroundRepeat: "no-repeat",
+              margin: "0 auto",
+              padding: "5px",
+              width: "60px",
+            }}
+          >
             {value}
           </div>
         </SliderPrimitive.Thumb>
       </SliderPrimitive.Root>
+      <div></div>
     </div>
   );
 });
