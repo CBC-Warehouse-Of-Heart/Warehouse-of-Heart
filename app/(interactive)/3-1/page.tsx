f37eb01eae83e4d013eacb48d61ffaee28b51f07
LittleNa1000 <naphanccnp@gmail.com>
2023-12-25 14:24:43 +0700

    feat: add 4-4 and modify text styles

--- app/(interactive)/3-1/page.tsx
+++ app/(interactive)/3-1/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Page = (props: Props) => {
   return (
     <>
-      <Link href="/3-2">
+      <Link href="/4-1">
         <Image
           src="/images/bg_3-1.webp"
           alt="Description"
@@ -14,16 +14,16 @@ const Page = (props: Props) => {
           className="-z-1"
         />
         <div className="flex h-screen flex-col items-center justify-center">
-          <div className="container mb-[220px] mt-[244px] px-10 text-center ">
-            <p className="text-xl leading-normal text-[#F8F8F7] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+          <div className="container px-10 text-center ">
+            <p className="text-3xl leading-loose text-[#F8F8F7] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
               ทำไมถึงมาอยู่ตรงนี้กันนะ <br /> ไม่ได้เห็นมานานเลย..
             </p>
           </div>
-          <div className="container z-10 text-center">
-            <p className=" text-base leading-normal text-[#F8F8F7] ">
-              กดที่หน้าจอเพื่อไปต่อ
-            </p>
-          </div>
+        </div>
+        <div className="container fixed left-1/2 top-[85%] z-10 -translate-x-1/2 transform text-center">
+          <p className=" text-base leading-normal text-[#F8F8F7] ">
+            กดที่หน้าจอเพื่อไปต่อ
+          </p>
         </div>
       </Link>
     </>
