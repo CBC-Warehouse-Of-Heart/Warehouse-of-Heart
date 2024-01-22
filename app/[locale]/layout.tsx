5cd8715897849841d7fd506ca6c84f68a089db4f
tontan2545 <tontan2545@gmail.com>
2024-01-22 13:44:24 +0700

    style: routing

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,8 @@
+import { locales } from "@/lib/i18n";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { IBM_Plex_Sans_Thai_Looped, Lora } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 
 const ibmPlexSansThaiLooped = IBM_Plex_Sans_Thai_Looped({
   weight: ["400", "500", "600", "700"],
@@ -20,13 +21,19 @@ export const metadata: Metadata = {
   description: "Warehouse of Heart",
 };
 
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
 export default function RootLayout({
   children,
+  params: { locale },
 }: {
   children: React.ReactNode;
+  params: { locale: string };
 }) {
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body
         className={cn(
           lora.variable,
