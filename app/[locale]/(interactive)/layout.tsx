38485dddaf0d9fddf9132f16cf085ee6ae9d4fd7
tontan2545 <tontan2545@gmail.com>
2024-02-04 15:01:10 +0700

    feat: fix merge commit

--- app/[locale]/(interactive)/layout.tsx
+++ app/[locale]/(interactive)/layout.tsx
@@ -24,22 +24,26 @@ export default function RootLayout({
         "relative h-full w-full bg-[radial-gradient(80.99%_50%_at_50%_50%,#AA5656_0%,#CE9A87_51.04%,#F2DEB9_100%)]"
       }
     >
-      <div className="fixed right-5 top-20 flex items-center">
+      <div className="fixed right-5 top-5 z-10 flex items-center">
         <Link href={path} locale={locale === "en" ? "th" : "en"}>
-          <Button variant="ghost" className="h-auto w-auto p-3 text-accent">
+          <Button
+            variant="ghost"
+            className="h-auto w-auto p-3 text-accent"
+            size="sm"
+          >
             {locale === "en" ? "EN" : "TH"}
           </Button>
         </Link>
-        <Separator orientation="vertical" className="h-10" />
+        <Separator orientation="vertical" className="h-8" />
         <Button
           variant="ghost"
           onClick={handleToggleSound}
-          className="h-auto w-auto p-3 text-accent"
+          className="h-auto w-auto rounded-sm p-2 text-accent"
         >
           {sound ? (
-            <Volume2 className="h-7 w-7" />
+            <Volume2 className="h-5 w-5" />
           ) : (
-            <VolumeX className="h-7 w-7" />
+            <VolumeX className="h-5 w-5" />
           )}
         </Button>
       </div>
