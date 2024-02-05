0a8c24a4a99b0e639b812c7452a8e33828ad91bc
RataSurithpinyo <rataprim30026@gmail.com>
2024-02-05 11:56:16 +0700

    update nextButton to support i18n

--- components/ui/nextButton.tsx
+++ components/ui/nextButton.tsx
@@ -1,30 +1,18 @@
+import { useTranslations } from "next-intl";
 import { Button } from "./button";
 
-const NextButton = () => {
+type Props = {
+  trigger?: boolean;
+  label?: string;
+};
+
+const NextButton = ({ trigger = true, label }: Props) => {
+  const t = useTranslations("NextButton");
   return (
     <div className="fixed left-1/2 top-[60%] -translate-x-1/2 transform">
-      <Button className="relative" variant="next" size="xs">
-        <p className="fixed left-1/2 -translate-x-1/2 transform text-base font-semibold text-[#6C1F1F]">
-          ถัดไป
-        </p>
-        <div className="absolute right-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 20 20"
-            fill="none"
-          >
-            <path
-              d="M8.33337 5.83334L12.5 10L8.33337 14.1667"
-              stroke="#6C1F1F"
-              stroke-width="1.66667"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </div>
-      </Button>
+    <Button className="relative" disabled={!trigger} variant="next" size="xs">
+      <p className="fixed left-1/2 -translate-x-1/2 transform text-base font-semibold text-[#6C1F1F]">{label ? label : t("next")}</p>
+    </Button>
     </div>
   );
 };
