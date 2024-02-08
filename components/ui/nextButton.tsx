5f141b6e0eb8d2375709f0d56a5a4b77634a50cb
YorJor <korakrit.vic@gmail.com>
2024-02-08 22:37:40 +0700

    current scene 3 4 5

--- components/ui/nextButton.tsx
+++ components/ui/nextButton.tsx
@@ -1,14 +1,19 @@
+import { useTranslations } from "next-intl";
+
 const NextButton = (props: { trigger: boolean }) => {
-    return (
-      <button
-        // ถ้า props ที่ได้มาเป็น false => disable
-        disabled={!props.trigger}
-        // ใส่ disabled:text-[#B8BFCB]
-        className="relative flex h-8 w-[100px] items-center justify-center rounded-[100px] bg-[#F8F8F7] text-[#6C1F1F] disabled:text-[#B8BFCB]"
-      >
-        <p className="text-base font-semibold">ถัดไป</p>
-      </button>
-    );
-  };
-  
-  export default NextButton;
+
+  const b = useTranslations("NextButton");
+
+  return (
+    <button
+      // ถ้า props ที่ได้มาเป็น false => disable
+      disabled={!props.trigger}
+      // ใส่ disabled:text-[#B8BFCB]
+      className="relative flex h-8 w-[100px] items-center justify-center rounded-[100px] bg-[#F8F8F7] text-[#6C1F1F] disabled:text-[#B8BFCB]"
+    >
+      <p className="text-base font-semibold">{b("next")}</p>
+    </button>
+  );
+};
+
+export default NextButton;
