f042dbfdd8cc5a6b086352418c8c51294d43fa93
tontan2545 <tontan2545@gmail.com>
2024-02-09 20:38:54 +0700

    feat: redirect to s3 for image storage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 const withNextIntl = require("next-intl/plugin");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withNextIntl("./lib/i18n.ts")({});
+const nextConfig = withNextIntl("./lib/i18n.ts")({
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: "warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com",
+      },
+    ],
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/img/:path*",
+        destination:
+          "https://warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com/img/:path*",
+        permanent: true,
+      },
+      {
+        source: "/postcards/:path*",
+        destination:
+          "https://warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com/postcards/:path*",
+        permanent: true,
+      },
+      {
+        source: "/sound/:path*",
+        destination:
+          "https://warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com/sound/:path*",
+        permanent: true,
+      },
+    ];
+  },
+});
 
 module.exports = nextConfig;
