5b8786f79052d6df0cf0ece02bf4a34820f41cf7
tontan2545 <tontan2545@gmail.com>
2024-02-09 22:15:02 +0700

    feat: move caching to cloudfare cdn

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = withBundleAnalyzer(
       unoptimized: true,
       remotePatterns: [
         {
-          hostname: "warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com",
+          hostname: "woh-cdn.coachbychamp.com",
         },
       ],
     },
@@ -18,20 +18,17 @@ const nextConfig = withBundleAnalyzer(
       return [
         {
           source: "/img/:path*",
-          destination:
-            "https://warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com/img/:path*",
+          destination: "https://woh-cdn.coachbychamp.com/img/:path*",
           permanent: true,
         },
         {
           source: "/postcards/:path*",
-          destination:
-            "https://warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com/postcards/:path*",
+          destination: "https://woh-cdn.coachbychamp.com/postcards/:path*",
           permanent: true,
         },
         {
           source: "/sound/:path*",
-          destination:
-            "https://warehouse-of-heart-static.s3.ap-southeast-1.amazonaws.com/sound/:path*",
+          destination: "https://woh-cdn.coachbychamp.com/sound/:path*",
           permanent: true,
         },
       ];
