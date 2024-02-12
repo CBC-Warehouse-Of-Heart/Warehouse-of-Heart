41ef34b5a6e8dc69ae6762003d0744d6ec7374d5
matikant <fe-2003@hotmail.com>
2024-02-12 21:14:41 +0700

    fix: text position

--- app/[locale]/(interactive)/1-1/page.tsx
+++ app/[locale]/(interactive)/1-1/page.tsx
@@ -8,12 +8,12 @@ const Page = (props: Props) => {
   const t = useTranslations("1-1");
   return (
     <div className="absolute flex h-full w-full flex-col items-center bg-1-1 bg-cover bg-no-repeat text-center text-base">
-      <p className="mt-[24vh] font-bold text-woh-black">{t("about")}</p>
+      <p className="mt-[28vh] font-bold text-woh-black">{t("about")}</p>
       <p className="mt-2 w-80 text-woh-black">{t("story")}</p>
       <p className="mt-10 font-bold text-[#A65554]">{t("warningHeading")}</p>
       <p className="mt-2 w-72 text-[#A65554]">{t("warning")}</p>
 
-      <div className="absolute bottom-[21vh]">
+      <div className="absolute bottom-[10vh]">
         <Link href="/1-2">
           <NextButton />
         </Link>
